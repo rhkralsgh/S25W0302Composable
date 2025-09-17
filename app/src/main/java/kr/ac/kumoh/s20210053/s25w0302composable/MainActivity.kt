c0d026e13221d11ce3def4bd53adae9481723123
rhkralsgh <kmh23937@gmail.com>
2025-09-17 15:33:32 +0900

    🎉Project 시작

---
+++ app/src/main/java/kr/ac/kumoh/s20210053/s25w0302composable/MainActivity.kt
@@ -0,0 +1,38 @@
+package kr.ac.kumoh.s20210053.s25w0302composable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.s20210053.s25w0302composable.ui.theme.S25W0302ComposableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S25W0302ComposableTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            
+        }
+    }
+}
